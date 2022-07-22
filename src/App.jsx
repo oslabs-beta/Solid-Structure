9a66cc81d975f25d7ddcb9de14a6d064086b8808
Brandon Brighi <brandon.erosenvironmental@gmail.com>
2022-07-22 11:00:45 -0700

    updated logmonitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import { Show, createSignal, createRoot, getOwner } from 'solid-js';
-import { Header } from "./components/Header";
-import { Inspect } from "./components/Inspect";
-import { Graph } from "./components/Graph";
-import { LogMonitor } from "./components/LogMonitor";
-import { Navbar } from "./components/Navbar";
+import { Header } from './components/Header';
+import { Inspect } from './components/Inspect';
+import { Graph } from './components/Graph';
+import { LogMonitor } from './components/LogMonitor';
+import { Navbar } from './components/Navbar';
 import './styles/main.scss';
 
 export const SolidStructure = (props) => {
-  // const [children, root] = createRoot(() => [props.children, getOwner()!]); 
-  const [tab, setTab] = createSignal("inspector"); // "inspector", "graph", "logmonitor"
-  const [cache, setCache] = createSignal({}); //creating signal for inspect, need to act as a reset or refresh or all graphs 
+  // const [children, root] = createRoot(() => [props.children, getOwner()!]);
+  const [tab, setTab] = createSignal('inspector'); // "inspector", "graph", "logmonitor"
+  const [cache, setCache] = createSignal({}); //creating signal for inspect, need to act as a reset or refresh or all graphs
 
   return (
     <>
       <Header />
       <div id="display">
-        <Show when={tab() === "inspector"}>
-          <Inspect cache={cache()} setCache={setCache}/>
+        <Show when={tab() === 'inspector'}>
+          <Inspect cache={cache()} setCache={setCache} />
           <Graph />
         </Show>
 
-        <Show when={tab() === "graph"}>
+        <Show when={tab() === 'graph'}>
           <div id="randomContainer"></div>
           <Graph />
         </Show>
 
-        {/* <Show when={tab() === "logmonitor"}>
+        <Show when={tab() === 'logmonitor'}>
           <LogMonitor />
-        </Show> */}
+        </Show>
       </div>
-      <Navbar setTab={setTab}/> 
+      <Navbar setTab={setTab} />
     </>
-  )
-}
+  );
+};
