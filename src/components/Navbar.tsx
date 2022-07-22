1e83e7aa4e4f760cdd3bfbaf713f954fb2e5b9a1
Brandon Brighi <brandon.erosenvironmental@gmail.com>
2022-07-22 12:34:10 -0700

    created tsx file for Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { Component, JSX } from 'solid-js';
+
+import '../styles/components/_navbar.scss';
+
+// type PropTypes = {
+//   setTab: (fn: (btn: string) => string) => void;
+// };
+// type Component<P = { setTab: (fn: (btn: string) => string) => void }> = (
+//   props: P
+// ) => JSX.Element;
+type Component<P = { setTab: (fn: (btn: string) => string) => void }> = (
+  props: P
+) => {};
+
+type TabStrings = 'inspector' | 'graph' | 'logmonitor';
+
+type TabArray = TabStrings[];
+
+export const Navbar: Component = (props) => {
+  const handleClick: JSX.EventHandler<HTMLInputElement, MouseEvent> = (e) => {
+    const tabs: TabArray = [];
+    props.setTab((btn) => {
+      if (e.target.id === 'inspector') {
+        btn = 'inspector';
+      } else if (e.target.id === 'graph') {
+        btn = 'graph';
+      } else if (e.target.id === 'logmonitor') {
+        btn = 'logmonitor';
+      }
+      tabs.forEach((tab: string) => {
+        btn !== tab
+          ? document.getElementById(tab).classList.remove('active')
+          : document.getElementById(tab).classList.add('active');
+      });
+      return btn;
+    });
+  };
+  return (
+    <div id="navbar">
+      <div id="navbar-btns">
+        <div
+          class="tab active"
+          type="button"
+          id="inspector"
+          onClick={handleClick}
+        >
+          Inspector
+        </div>
+        <div class="tab" type="button" id="graph" onClick={handleClick}>
+          Graph
+        </div>
+        <div class="tab" type="button" id="logmonitor" onClick={handleClick}>
+          Log Monitor
+        </div>
+      </div>
+    </div>
+  );
+};
