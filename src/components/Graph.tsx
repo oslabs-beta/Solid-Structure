1e557e492aa9131f7627c73021a03b4296dcfabb
yermie <yermolovpersonal@gmail.com>
2022-07-23 10:15:46 -0700

    Converted Graph component to Typescript

---
+++ src/components/Graph.tsx
@@ -0,0 +1,15 @@
+// import '../styles/components/_inspector.scss';
+
+import type { Component } from 'solid-js';
+
+//afaik this should work
+
+export const Graph: Component = () => {
+
+  return(
+    <div id="graphContainer">
+      <div id="containerDep"></div>
+      <div id="containerStr"></div>
+    </div>
+  )
+};
