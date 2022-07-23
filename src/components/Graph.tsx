d716c8be4aff6ca835623df02cc18c80b3b031ac
Brandon Brighi <brandon.erosenvironmental@gmail.com>
2022-07-23 12:00:57 -0700

    Co-authored-by: Tanner Lyon <THLyon@users.noreply.github.com>

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -4,14 +4,11 @@ import type { Component } from 'solid-js';
 
 //afaik this should work
 
-const Graph: Component = () => {
-
-  return(
+export const Graph: Component = () => {
+  return (
     <div id="graphContainer">
       <div id="containerDep"></div>
       <div id="containerStr"></div>
     </div>
-  )
+  );
 };
-
-export default Graph;
