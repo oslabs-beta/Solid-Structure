2fb84be996a572ad9cda4dbc1790d2585e9f493e
yermie <yermolovpersonal@gmail.com>
2022-07-25 10:30:56 -0700

    Just working with d3

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,8 +1,28 @@
 // import '../styles/components/_inspector.scss';
-
+import * as d3 from "d3";
 import type { Component } from 'solid-js';
 
-//afaik this should work
+
+
+const el = d3.select('#graphContainer');
+
+const width = 960, height = 500;
+const x_scale = d3.scaleBand().range([0, width])
+const y_scale = d3.scaleLinear().range([height, 0])
+
+d3.json("./random.json").then(({data}) => {
+  el
+ .selectAll("rect")
+ .data(data)
+ .join("rect")
+  .attr("class", "bar")
+  .attr("x", (d: any) => x_scale(d.Name))
+  .attr("y", (d: any) => y_scale(d.Population))
+  .attr("width", x_scale.bandwidth())
+  .attr("height", (d) => height - y_scale(d.Population));
+});
+
+
 
 const Graph: Component = () => {
 
