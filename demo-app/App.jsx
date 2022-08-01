fce922adea587cef068ff2932fe1ba34d1c489ed
Matthew Yeon <m.jiwooyeon@gmail.com>
2022-08-01 17:26:03 -0400

    Minor fixes before merging with D3 zoom

--- demo-app/App.jsx
+++ demo-app/App.jsx
@@ -1,18 +1,25 @@
-import { createSignal } from "solid-js";
+import { createSignal, createRoot, getOwner } from "solid-js";
 import './style.css';
 
-export const App = () => {
+export const App = (props) => {
     const [count, setCount] = createSignal(0); 
+    const [bool, setBool] = createSignal(true); 
+    const [guess, setGuess] = createSignal("boo"); 
 
     const increment = () => setCount(count() + 1);
     const decrement = () => setCount(count() - 1); 
+    const updateGuess = () => setGuess(guess()+"hi"); 
     
+    const [children, root] = createRoot(() => [props.children, getOwner()]);
+    console.log(root);
+
     return (
       <div id='counter'>
         <div id='counterBox'>{count}</div>
         <div>
           <button id="decrementBtn" onClick={decrement} type="button"> Decrement </button>
           <button id="incrementBtn" onClick={increment} type="button"> Increment </button>
+          <button id="incrementBtn" onClick={updateGuess} type="button"> UpdateGuess </button>
         </div>
       </div>
     );
--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -9,7 +9,7 @@ import './styles/main.scss';
 
 export const SolidStructure: SolidComponent = (props) => {
   const [children, root] = createRoot(() => [props.children, getOwner()]);
-  console.log(root);
+  console.log("App.tsx/root:", root);
   const [tab, setTab] = createSignal<TabType>('inspector');
   const [orientation, setOrientation] = createSignal<OrientType>('horizontal');
   const [record, setRecord] = createSignal<boolean>(true);
@@ -43,33 +43,36 @@ export const SolidStructure: SolidComponent = (props) => {
   });
 
   return (
-    <div id="mainApp">
-      <Header
-        tab={tab}
-        orientation={orientation}
-        setOrientation={setOrientation}
-      />
-      <div id="mainDisplay">
-        <Switch>
-          <Match when={tab() === 'inspector'}>
-            <SignalList root={root} />
-            <div class="line inspc" onMouseDown={() => setOnDrag(true)}></div>
-            <Graph tab={tab} orientation={orientation} boxsize={boxsize} />
-          </Match>
-          <Match when={tab() === 'graph'}>
-            <Graph tab={tab} orientation={orientation} />
-          </Match>
-          <Match when={tab() === 'logmonitor'}>
-            <LogMonitor
-              record={record}
-              setRecord={setRecord}
-              caches={caches}
-              setCaches={setCaches}
-            />
-          </Match>
-        </Switch>
+    <>
+      {children}
+      <div id="mainApp">
+        <Header
+          tab={tab}
+          orientation={orientation}
+          setOrientation={setOrientation}
+        />
+        <div id="mainDisplay">
+          <Switch>
+            <Match when={tab() === 'inspector'}>
+              <SignalList root={root} />
+              <div class="line inspc" onMouseDown={() => setOnDrag(true)}></div>
+              <Graph tab={tab} orientation={orientation} boxsize={boxsize} />
+            </Match>
+            <Match when={tab() === 'graph'}>
+              <Graph tab={tab} orientation={orientation} />
+            </Match>
+            <Match when={tab() === 'logmonitor'}>
+              <LogMonitor
+                record={record}
+                setRecord={setRecord}
+                caches={caches}
+                setCaches={setCaches}
+              />
+            </Match>
+          </Switch>
+        </div>
+        <Navbar tab={tab} setTab={setTab} />
       </div>
-      <Navbar tab={tab} setTab={setTab} />
-    </div>
+    </>
   );
 };
